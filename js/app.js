357303d0aa76dc72d0808182e08d28d0d5555559
jokay03J-V2 <jmagro@edenschool.fr>
2022-11-17 11:15:19 +0100

    update js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 import "../css/style.css"
+const wordContainer = document.getElementById("word")
+let iWord = 0
+let iLetters = 0;
+const words = ["design", "frontend", "backend", "testing"]
+
+setInterval(() => {
+    let word = words[iWord]
+    if (iLetters === word.length) {
+        iLetters = 0;
+        iWord++
+        wordContainer.innerText = ""
+    }
+    if (iWord === words.length) {
+        iWord = 0;
+        iLetters = 0;
+    }
+
+    word = words[iWord]
+    wordContainer.innerText += word[iLetters]
+    iLetters++
+}, 500);
